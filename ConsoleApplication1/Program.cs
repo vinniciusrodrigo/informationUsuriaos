b11048b059ee7a61406dd22eb755db97a4f3be86
Aluno <aluno@naverecife.local>
2019-03-30 14:16:33 -0300

    Adiocionei novo programa para capturar informacoes de usuarios

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[10];
+            Console.WriteLine("Digite 10 valores:");
+            for (int i = 0; i < 10; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                valores[i] = a; 
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(valores[i]);
+            }
+        }
+    }
+}
